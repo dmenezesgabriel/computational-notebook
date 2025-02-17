3420590014021a1089a80bb5b3956d9c0634cfbf
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-02-17 17:56:06 -0300

    chore: add github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/computational-notebook",
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.md"],
 });
