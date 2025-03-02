4e04f3520956d541802a25fbc577763c2777182c
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-03-01 21:53:28 -0300

    chore: update sidebar burger menu to default button component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { MenuIcon } from "lucide-react";
+import { Button } from "./button";
 
 interface HeaderProps {
   onToggleMenuClick: () => void;
@@ -9,10 +10,12 @@ export function Header({ onToggleMenuClick }: HeaderProps) {
     <header>
       <div className="flex flex-row px-5 py-2 items-center">
         <div className="flex flex-row items-center space-x-2">
-          <div className="hover:cursor-pointer" onClick={onToggleMenuClick}>
+          <Button.Root variant="secondary" onClick={onToggleMenuClick}>
             <MenuIcon className="w-4 h-4" />
-          </div>
-          <h1>Computational Notebooks</h1>
+          </Button.Root>
+          <span className="text-lg text-slate-700">
+            Computational Notebooks
+          </span>
         </div>
       </div>
     </header>
